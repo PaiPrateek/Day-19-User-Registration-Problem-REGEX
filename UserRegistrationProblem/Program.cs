ce646a460ec0016b603bb671f16117d7931995fa
Prateek Pai <paiprateek123@gmail.com>
2022-07-09 10:49:10 +0530

    Welcome to User Registration Problem

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem");
+        }
+    }
+}
